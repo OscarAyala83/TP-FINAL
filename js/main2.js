d61e9b113f03c01653cb83463a505dcdbe8eddfc
Oscar Ayala <ripamar@hotmail.com>
2022-10-05 16:09:46 -0300

    LocalStorage

--- js/main2.js
+++ js/main2.js
@@ -1,58 +1,79 @@
- document.querySelector('#agregar').addEventListener('click', agregar);
-
+document.querySelector('#agregar').addEventListener('click', agregar);
+document.querySelector('#historial').addEventListener('click', registro)
+document.querySelector('#borrar').addEventListener('click', borrarRegistro)
 const aNuevoCamion = [];
 
-function agregar(){
+
+function agregar() {
 
 
-nCartaPorte = document.getElementById('CP').value;   
-nKilos = document.querySelector("#kilos").value;
-nHumedad = document.getElementById('humedad').value;
-nMerma = document.getElementById('TotalMermaKilos').innerText;
-nKilosNeto = document.getElementById('KilosNetos').innerText;
-aNuevoCamion.push({nCartaPorte, nKilos, nHumedad, nMerma, nKilosNeto}); 
-console.log (aNuevoCamion)
-/* console.log (nCartaPorte, nKilos, nHumedad, nMerma, nKilosNeto);   */
-const tablaCamiones = document.getElementById('listadoCamiones');
+    CartaPorte = document.getElementById('CP').value;
+    Kilos = document.querySelector("#kilos").value;
+    Humedad = document.getElementById('humedad').value;
+    Merma = document.getElementById('TotalMermaKilos').innerText;
+    KilosNeto = document.getElementById('KilosNetos').innerText;
+    aNuevoCamion.push({ CartaPorte, Kilos, Humedad, Merma, KilosNeto });
 
-let fila = tablaCamiones.insertRow(0+1);
-let celda1 = fila.insertCell(0);
-let celda2 = fila.insertCell(1);
-let celda3 = fila.insertCell(2);
-let celda4 = fila.insertCell(3);
-let celda5 = fila.insertCell(4);
-let celda6 = fila.insertCell(5);
+    localStorage.setItem('lista', JSON.stringify(aNuevoCamion));
 
-celda1.innerHTML = nCartaPorte;
-celda2.innerHTML = nKilos;
-celda3.innerHTML = nHumedad;
-celda4.innerHTML = nMerma;
-celda5.innerHTML = nKilosNeto;
-celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');
 
-document.querySelector('#borrarRegistro').addEventListener('click', confirmacion);
+    const tablaCamiones = document.getElementById('listadoCamiones');
+
+    let fila = tablaCamiones.insertRow(0 + 1);
+    let celda1 = fila.insertCell(0);
+    let celda2 = fila.insertCell(1);
+    let celda3 = fila.insertCell(2);
+    let celda4 = fila.insertCell(3);
+    let celda5 = fila.insertCell(4);
+    let celda6 = fila.insertCell(5);
+
+    celda1.innerHTML = CartaPorte;
+    celda2.innerHTML = Kilos;
+    celda3.innerHTML = Humedad;
+    celda4.innerHTML = Merma;
+    celda5.innerHTML = KilosNeto;
+    celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');
+
+
+    document.querySelector('#borrarRegistro').addEventListener('click', confirmacion);
 
 }
+function registro() {
+
+    if (localStorage.getItem('lista') === null) {
+        alert("El Historial esta vacio")
+    } else {
+        alert(localStorage.getItem('lista'));
+    }
+
+    //celda2.innerHTML = JSON.parse(localStorage.getItem('nKilos'));
+}
+
+
 function confirmacion() {
-    let resultado = window.confirm ("Esta seguro que quiere eliminar este registro");
-    if (resultado===true ){
+    let resultado = window.confirm("Esta seguro que quiere eliminar este registro");
+    if (resultado === true) {
         eliminar()
+
     }
 
 }
 function eliminar() {
+    
+    let indice = aNuevoCamion.indexOf();
+    if (indice == -1) {
+        aNuevoCamion.splice(indice, 1);
+        document.getElementById('listadoCamiones').deleteRow(1);
 
-const indice = aNuevoCamion.indexOf();
-
-if (indice ==-1) {
-    aNuevoCamion.splice(indice,1);
-    console.log (indice);
-    console.log (aNuevoCamion);
-    document.getElementById('listadoCamiones').deleteRow(1);
+    }
+    
 }
+function borrarRegistro() {
+    let resultado = window.confirm("Esta seguro que quiere eliminar el historial");
+    if (resultado === true) {
+        localStorage.clear()
 
+    }
 }
-     
-
 
 
