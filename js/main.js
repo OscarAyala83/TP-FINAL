ab16ed923287c46d8e402876c97666945d48ce0b
Oscar Ayala <ripamar@hotmail.com>
2022-10-14 15:41:39 -0300

    Cereales agregados

--- js/main.js
+++ js/main.js
@@ -1,75 +1,40 @@
-
-const soja = [
-   { humedad: 13.6, rebaja: 0.69 }, { humedad: 17.5, rebaja: 5.17 }, { humedad: 21.4, rebaja: 9.66 },
-   { humedad: 13.7, rebaja: 0.80 }, { humedad: 17.6, rebaja: 5.29 }, { humedad: 21.5, rebaja: 9.77 },
-   { humedad: 13.8, rebaja: 0.92 }, { humedad: 17.7, rebaja: 5.40 }, { humedad: 21.6, rebaja: 9.89 },
-   { humedad: 13.9, rebaja: 1.03 }, { humedad: 17.8, rebaja: 5.52 }, { humedad: 21.7, rebaja: 10.00 },
-   { humedad: 14.0, rebaja: 1.15 }, { humedad: 17.9, rebaja: 5.63 }, { humedad: 21.8, rebaja: 10.11 },
-   { humedad: 14.1, rebaja: 1.26 }, { humedad: 18.0, rebaja: 5.75 }, { humedad: 21.9, rebaja: 10.23 },
-   { humedad: 14.2, rebaja: 1.38 }, { humedad: 18.1, rebaja: 5.86 }, { humedad: 22.0, rebaja: 10.34 },
-   { humedad: 14.3, rebaja: 1.49 }, { humedad: 18.2, rebaja: 5.98 }, { humedad: 22.1, rebaja: 10.46 },
-   { humedad: 14.4, rebaja: 1.61 }, { humedad: 18.3, rebaja: 6.09 }, { humedad: 22.2, rebaja: 10.57 },
-   { humedad: 14.5, rebaja: 1.72 }, { humedad: 18.4, rebaja: 6.21 }, { humedad: 22.3, rebaja: 10.69 },
-   { humedad: 14.6, rebaja: 1.84 }, { humedad: 18.5, rebaja: 6.32 }, { humedad: 22.4, rebaja: 10.80 },
-   { humedad: 14.7, rebaja: 1.95 }, { humedad: 18.6, rebaja: 6.44 }, { humedad: 22.5, rebaja: 10.92 },
-   { humedad: 14.8, rebaja: 2.07 }, { humedad: 18.7, rebaja: 6.55 }, { humedad: 22.6, rebaja: 11.03 },
-   { humedad: 14.9, rebaja: 2.18 }, { humedad: 18.8, rebaja: 6.67 }, { humedad: 22.7, rebaja: 11.15 },
-   { humedad: 15.0, rebaja: 2.30 }, { humedad: 18.9, rebaja: 6.78 }, { humedad: 22.8, rebaja: 11.26 },
-   { humedad: 15.1, rebaja: 2.41 }, { humedad: 19.0, rebaja: 6.90 }, { humedad: 22.9, rebaja: 11.38 },
-   { humedad: 15.2, rebaja: 2.53 }, { humedad: 19.1, rebaja: 7.01 }, { humedad: 23.0, rebaja: 11.49 },
-   { humedad: 15.3, rebaja: 2.64 }, { humedad: 19.2, rebaja: 7.13 }, { humedad: 23.1, rebaja: 11.61 },
-   { humedad: 15.4, rebaja: 2.76 }, { humedad: 19.3, rebaja: 7.24 }, { humedad: 23.2, rebaja: 11.72 },
-   { humedad: 15.5, rebaja: 2.87 }, { humedad: 19.4, rebaja: 7.36 }, { humedad: 23.3, rebaja: 11.84 },
-   { humedad: 15.6, rebaja: 2.99 }, { humedad: 19.5, rebaja: 7.47 }, { humedad: 23.4, rebaja: 11.95 },
-   { humedad: 15.7, rebaja: 3.10 }, { humedad: 19.6, rebaja: 7.59 }, { humedad: 23.5, rebaja: 12.07 },
-   { humedad: 15.8, rebaja: 3.22 }, { humedad: 19.7, rebaja: 7.70 }, { humedad: 23.6, rebaja: 12.18 },
-   { humedad: 15.9, rebaja: 3.33 }, { humedad: 19.8, rebaja: 7.82 }, { humedad: 23.7, rebaja: 12.30 },
-   { humedad: 16.0, rebaja: 3.45 }, { humedad: 19.9, rebaja: 7.93 }, { humedad: 23.8, rebaja: 12.41 },
-   { humedad: 16.1, rebaja: 3.56 }, { humedad: 20.0, rebaja: 8.05 }, { humedad: 23.9, rebaja: 12.53 },
-   { humedad: 16.2, rebaja: 3.68 }, { humedad: 20.1, rebaja: 8.16 }, { humedad: 24.0, rebaja: 12.64 },
-   { humedad: 16.3, rebaja: 3.79 }, { humedad: 20.2, rebaja: 8.28 }, { humedad: 24.1, rebaja: 12.76 },
-   { humedad: 16.4, rebaja: 3.91 }, { humedad: 20.3, rebaja: 8.39 }, { humedad: 24.2, rebaja: 12.87 },
-   { humedad: 16.5, rebaja: 4.02 }, { humedad: 20.4, rebaja: 8.51 }, { humedad: 24.3, rebaja: 12.99 },
-   { humedad: 16.6, rebaja: 4.14 }, { humedad: 20.5, rebaja: 8.62 }, { humedad: 24.4, rebaja: 13.10 },
-   { humedad: 16.7, rebaja: 4.25 }, { humedad: 20.6, rebaja: 8.74 }, { humedad: 24.5, rebaja: 13.22 },
-   { humedad: 16.8, rebaja: 4.37 }, { humedad: 20.7, rebaja: 8.85 }, { humedad: 24.6, rebaja: 13.33 },
-   { humedad: 16.9, rebaja: 4.48 }, { humedad: 20.8, rebaja: 8.97 }, { humedad: 24.7, rebaja: 13.45 },
-   { humedad: 17.0, rebaja: 4.60 }, { humedad: 20.9, rebaja: 9.08 }, { humedad: 24.8, rebaja: 13.56 },
-   { humedad: 17.1, rebaja: 4.71 }, { humedad: 21.0, rebaja: 9.20 }, { humedad: 24.9, rebaja: 13.68 },
-   { humedad: 17.2, rebaja: 4.83 }, { humedad: 21.1, rebaja: 9.31 }, { humedad: 25.0, rebaja: 13.79 },
-   { humedad: 17.3, rebaja: 4.94 }, { humedad: 21.2, rebaja: 9.43 },
-   { humedad: 17.4, rebaja: 5.06 }, { humedad: 21.3, rebaja: 9.54 }
-
-];
-
-
 document.getElementById('form')
    .addEventListener('submit', function (e) {
       e.preventDefault();
-
-
-      const volatil = document.getElementById('volatil');
-      const kilos = document.querySelector("#kilos").value;
-      const humedadselec = document.getElementById('humedad').value;
-
-
+      datos();
+      if (cereal === 'soja') {
+        humedadSoja();
+      } 
+      if (cereal ==='trigo'){
+         humedadTrigo();
+      }
+               
+ function humedadSoja (){
+      
       if (humedadselec < 13.6) {
-         alert("Este camion no tiene humedad. Por favor ingrese un valor superior a 13.5")
+
+         Swal.fire({
+            icon: 'error',
+            title: 'Este camion se encuentra dentro de la tolerancia admitida'
+          })
       } else if (humedadselec > 25) {
-         alert("Este camion supuera el maximo permitido de Humedad")
+         Swal.fire({
+            icon: 'error',
+            title: 'Este camion supera el maximo permitido de humedad.'
+          })
       }
       if (volatil.checked == true) {
          mermaVolatil = 0.50;
-         calculo();
+         calculoSoja();
       } else {
          mermaVolatil = 0;
-         calculo();
+         calculoSoja();
       }
-
-      function calculo() {
+   }
+      function calculoSoja() {
          const bus = soja.find(elemento => elemento.humedad == humedadselec);
          const merma = bus.rebaja;
-
+         console.log (bus)
          Porcentaje.textContent = merma;
          MKilos = (Math.ceil(kilos * merma) / 100);
          MermaKilos.textContent = MKilos;
@@ -84,5 +49,53 @@ document.getElementById('form')
          nMerma = document.getElementById('TotalMermaKilos').innerHTML;
 
       }
+ })
+   
+ function humedadTrigo() {
 
-   })
+      if (humedadselec < 14.10) {
+
+         Swal.fire({
+            icon: 'error',
+            title: 'Este camion se encuentra dentro de la tolerancia admitida'
+         })
+      } else if (humedadselec > 25) {
+         Swal.fire({
+            icon: 'error',
+            title: 'Este camion supera el maximo permitido de humedad.'
+          })
+      } 
+      if (volatil.checked == true) {
+         mermaVolatil = 0.30;
+         calculoTrigo();
+      } else {
+         mermaVolatil = 0;
+         calculoTrigo();
+      }
+   }
+   function calculoTrigo() {
+      datos();
+      const buscar = trigo.find((elemento) => elemento.humedad == humedadselec);
+      const mermatrigo = buscar.rebaja;
+      Porcentaje.textContent = mermatrigo;
+      MKilos = (Math.ceil(kilos * mermatrigo) / 100);
+      MermaKilos.textContent = MKilos;
+      MermManipuleo = 0.10 + mermaVolatil;
+      MermaManipuleo.textContent = MermManipuleo;
+      KManipuleo = (kilos * MermManipuleo) / 100;
+      KilosManipuleo.textContent = KManipuleo;
+      TMermaKilos = (MKilos + KManipuleo);
+      TotalMermaKilos.textContent = TMermaKilos;
+      KilosNetos.textContent = Math.ceil(kilos - TMermaKilos);
+      KNetos = Math.ceil(kilos - TMermaKilos);
+      nMerma = document.getElementById('TotalMermaKilos').innerHTML;
+
+   }
+   function datos (){
+      volatil = document.getElementById('volatil');
+      kilos = document.querySelector("#kilos").value;
+      humedadselec = document.getElementById('humedad').value;
+      cereal = document.querySelector("#cereales").value;
+   }   
+
+   
--- js/main2.js
+++ js/main2.js
@@ -1,94 +1,118 @@
 document.querySelector('#agregar').addEventListener('click', agregar);
-document.querySelector('#historial').addEventListener('click', registro)
-document.querySelector('#borrar').addEventListener('click', borrarRegistro)
-const aNuevoCamion = [];
+//document.querySelector('#borrar').addEventListener('click', borrarRegistro)
+window.addEventListener("load", registro) 
+let aNuevoCamion = [];
+
+moment.locale("es");
+let fecha = moment().format('Do MMM YY');
+console.log(fecha);
 
 
 function agregar() {
-
-
     CartaPorte = document.getElementById('CP').value;
     Kilos = document.querySelector("#kilos").value;
     Humedad = document.getElementById('humedad').value;
     Merma = document.getElementById('TotalMermaKilos').innerText;
     KilosNeto = document.getElementById('KilosNetos').innerText;
-
-
-
-
-    aNuevoCamion.push({ CartaPorte, Kilos, Humedad, Merma, KilosNeto });
+    const cereal = document.querySelector("#cereales").value;
+    aNuevoCamion.push({ cereal, CartaPorte, Kilos, Humedad, Merma, KilosNeto });
 
     localStorage.setItem('lista', JSON.stringify(aNuevoCamion));
 
-
     const tablaCamiones = document.getElementById('listadoCamiones');
+    console.log(aNuevoCamion)
 
     let fila = tablaCamiones.insertRow(0 + 1);
-    let celda1 = fila.insertCell(0);
-    let celda2 = fila.insertCell(1);
-    let celda3 = fila.insertCell(2);
-    let celda4 = fila.insertCell(3);
-    let celda5 = fila.insertCell(4);
-    let celda6 = fila.insertCell(5);
+    let celda0 = fila.insertCell(0);
+    let celda1 = fila.insertCell(1);
+    let celda2 = fila.insertCell(2);
+    let celda3 = fila.insertCell(3);
+    let celda4 = fila.insertCell(4);
+    let celda5 = fila.insertCell(5);
+    let celda6 = fila.insertCell(6);
 
+    
+    celda0.innerHTML = cereal; 
     celda1.innerHTML = CartaPorte;
     celda2.innerHTML = Kilos;
     celda3.innerHTML = Humedad;
     celda4.innerHTML = Merma;
     celda5.innerHTML = KilosNeto;
     celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');
-
-
-
     document.querySelector('#borrarRegistro').addEventListener('click', confirmacion);
-
 }
-
 
 function registro() {
+    
+    if (localStorage.getItem('lista') !== null) {
+        aNuevoCamion = JSON.parse(localStorage.getItem('lista'));
+    } 
+    if (aNuevoCamion.length > 0){
+            const tablaCamiones = document.getElementById('listadoCamiones');
+            aNuevoCamion.forEach(({cereal, CartaPorte, Kilos, Humedad, Merma, KilosNeto}) => {
+            let fila = tablaCamiones.insertRow(0 + 1);
+            let celda0 = fila.insertCell(0);
+            let celda1 = fila.insertCell(1);
+            let celda2 = fila.insertCell(2);
+            let celda3 = fila.insertCell(3);
+            let celda4 = fila.insertCell(4);
+            let celda5 = fila.insertCell(5);
+            let celda6 = fila.insertCell(6);
 
-    let ls = localStorage.getItem('lista');
-    console.log(ls)
-    if (localStorage.getItem('lista') === null) {
-        alert("El Historial esta vacio")
-    } else {
-
-        for (let local in ls) {
-
-            alert(ls);
-            break;
-        }
-
-    }
-
+            celda0.innerHTML = cereal;
+            celda1.innerHTML = CartaPorte;
+            celda2.innerHTML = Kilos;
+            celda3.innerHTML = Humedad;
+            celda4.innerHTML = Merma;
+            celda5.innerHTML = KilosNeto;
+            celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');    
+        })
+        const newButtons = document.querySelectorAll('#borrarRegistro')
+        newButtons.forEach((button) => {
+            button.addEventListener('click', confirmacion);
+        })
+    }  
 
 }
-
-
 function confirmacion() {
-    let resultado = window.confirm("Esta seguro que quiere eliminar este registro");
-    if (resultado === true) {
-        eliminar()
-
-    }
+    Swal.fire({
+        title: 'Esta seguro que quiere eliminar este registro?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        heightAuto:'true'
+        
+      }).then((result) => {
+        if (result.isConfirmed) {eliminar()
+          Swal.fire(
+            'Elminado!',
+            'El registro ha sido eliminado.',
+            'success',
+            
+          )
+        }
+      })
+    //let resultado = window.confirm("Esta seguro que quiere eliminar este registro");
+    //if (resultado === true) {
+        
+    
 
 }
-function eliminar() {
-
+function eliminar() { 
     let indice = aNuevoCamion.indexOf();
     if (indice == -1) {
         aNuevoCamion.splice(indice, 1);
-        document.getElementById('listadoCamiones').deleteRow(1);
-
+        document.getElementById('listadoCamiones').deleteRow(indice);
+        localStorage.setItem('lista', JSON.stringify(aNuevoCamion))
     }
-
-}
-function borrarRegistro() {
+    }
+/* 
+    function borrarRegistro() {
     let resultado = window.confirm("Esta seguro que quiere eliminar el historial");
     if (resultado === true) {
+        
         localStorage.clear()
-
-    }
-}
-
-
+        
+    } */
+//}
