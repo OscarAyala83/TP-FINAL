85b8c1b5c71e0ba9e8241b7c7ad210189ba3bd29
Oscar Ayala <ripamar@hotmail.com>
2022-10-28 14:29:21 -0300

    TP final

--- js/main.js
+++ js/main.js
@@ -1,156 +1,145 @@
+let MKilo;
 document.getElementById('form')
    .addEventListener('submit', function (e) {
       e.preventDefault();
       datos();
       if (cereal === 'soja') {
-        humedadSoja();
-      } 
-      if (cereal ==='trigo'){
+         humedadSoja();
+      }
+      if (cereal === 'trigo') {
          humedadTrigo();
-      }else{
-         if (cereal ==='maiz'){
+      } else {
+         if (cereal === 'maiz') {
             humedadMaiz();
          }
       }
-               
- function humedadSoja (){
-      
-      if (humedadselec < 13.6) {
 
-         Swal.fire({
-            icon: 'error',
-            title: 'Este camion se encuentra dentro de la tolerancia admitida'
-          })
-      } else if (humedadselec > 25) {
-         Swal.fire({
-            icon: 'error',
-            title: 'Este camion supera el maximo permitido de humedad.'
-          })
+      function humedadSoja() {
+
+         if (humedadselec < 13.6) {
+
+            Swal.fire({
+               icon: 'error',
+               title: 'Este camion se encuentra dentro de la tolerancia admitida - Ingrese un valor entre 13.6 - 25'
+            })
+         } else if (humedadselec > 25) {
+            Swal.fire({
+               icon: 'error',
+               title: 'Este camion supera el maximo permitido de humedad.'
+            })
+         }
+         if (volatil.checked == true) {
+            mermaVolatil = 0.50;
+            calculoSoja();
+         } else {
+            mermaVolatil = 0;
+            calculoSoja();
+         }
       }
-      if (volatil.checked == true) {
-         mermaVolatil = 0.50;
-         calculoSoja();
-      } else {
-         mermaVolatil = 0;
-         calculoSoja();
-      }
-   }
+
       function calculoSoja() {
 
-    fetch('cereales.json')
-    .then( (res) => res.json())
-    .then( (data) => {   
-    console.log(data)
-    let bus = data.find(elemento => elemento.humedadsoja == humedadselec);
-    const merma = bus.rebajasoja;
-    Porcentaje.textContent = merma;
-    MKilos = (Math.ceil(kilos * merma) / 100);
-    MermaKilos.textContent = MKilos;
-    MermManipuleo = 0.25 + mermaVolatil;
-    MermaManipuleo.textContent = MermManipuleo;
-    KManipuleo = (kilos * MermManipuleo) / 100;
-    KilosManipuleo.textContent = KManipuleo;
-    TMermaKilos = (MKilos + KManipuleo);
-    TotalMermaKilos.textContent = TMermaKilos;
-    KilosNetos.textContent = Math.ceil(kilos - TMermaKilos);
-    KNetos = Math.ceil(kilos - TMermaKilos);
-    nMerma = document.getElementById('TotalMermaKilos').innerHTML;
-    })
-        
-
+         fetch('cereales.json')
+            .then((res) => res.json())
+            .then((data) => {
+               let bus = data.find(elemento => elemento.humedadsoja == humedadselec);
+               const merma = bus.rebajasoja;
+               MermManipuleo = 0.25 + mermaVolatil;
+               Porcentaje.textContent = merma;
+               MKilos = (Math.ceil(kilos * merma) / 100);
+               porcentajes()
+            })
       }
- })
-   
- function humedadTrigo() {
+   })
 
-      if (humedadselec < 14.10) {
+function humedadTrigo() {
 
-         Swal.fire({
-            icon: 'error',
-            title: 'Este camion se encuentra dentro de la tolerancia admitida'
-         })
-      } else if (humedadselec > 25) {
-         Swal.fire({
-            icon: 'error',
-            title: 'Este camion supera el maximo permitido de humedad.'
-          })
-      } 
-      if (volatil.checked == true) {
-         mermaVolatil = 0.30;
-         calculoTrigo();
-      } else {
-         mermaVolatil = 0;
-         calculoTrigo();
-      }
+   if (humedadselec < 14.10) {
+
+      Swal.fire({
+         icon: 'error',
+         title: 'Este camion se encuentra dentro de la tolerancia admitida Ingrese un valor entre 14.10 - 25'
+      })
+   } else if (humedadselec > 25) {
+      Swal.fire({
+         icon: 'error',
+         title: 'Este camion supera el maximo permitido de humedad.'
+      })
    }
-   function calculoTrigo() {
-      datos();
-      fetch('cereales.json')
-      .then( (res) => res.json())
-      .then( (data) => {   
-      let bus = data.find(elemento => elemento.humedadtrigo == humedadselec);
-      const merma = bus.rebajatrigo;
-      Porcentaje.textContent = merma;
-      MKilos = (Math.ceil(kilos * merma) / 100);
-      MermaKilos.textContent = MKilos;
-      MermManipuleo = 0.10 + mermaVolatil;
-      MermaManipuleo.textContent = MermManipuleo;
-      KManipuleo = (kilos * MermManipuleo) / 100;
-      KilosManipuleo.textContent = KManipuleo;
-      TMermaKilos = (MKilos + KManipuleo);
-      TotalMermaKilos.textContent = TMermaKilos;
-      KilosNetos.textContent = Math.ceil(kilos - TMermaKilos);
-      KNetos = Math.ceil(kilos - TMermaKilos);
-      nMerma = document.getElementById('TotalMermaKilos').innerHTML;
-      }
-   )}
-   function datos (){
-      volatil = document.getElementById('volatil');
-      kilos = document.querySelector("#kilos").value;
-      humedadselec = document.getElementById('humedad').value;
-      cereal = document.querySelector("#cereales").value;
-   }   
-
-   function humedadMaiz (){
-      
-      if (humedadselec < 14.6) {
-
-         Swal.fire({
-            icon: 'error',
-            title: 'Este camion se encuentra dentro de la tolerancia admitida'
-          })
-      } else if (humedadselec > 25) {
-         Swal.fire({
-            icon: 'error',
-            title: 'Este camion supera el maximo permitido de humedad.'
-          })
-      }
-      if (volatil.checked == true) {
-         mermaVolatil = 0.30;
-         calculoMaiz();
-      } else {
-         mermaVolatil = 0;
-         calculoMaiz();
-      }
+   if (volatil.checked == true) {
+      mermaVolatil = 0.30;
+      calculoTrigo();
+   } else {
+      mermaVolatil = 0;
+      calculoTrigo();
    }
-   function calculoMaiz() {
-      datos();
-      fetch('cereales.json')
-      .then( (res) => res.json())
-      .then( (data) => {   
-      let bus = data.find(elemento => elemento.humedadmaiz == humedadselec);
-      const merma = bus.rebajamaiz;
-      Porcentaje.textContent = merma;
-      MKilos = (Math.ceil(kilos * merma) / 100);
-      MermaKilos.textContent = MKilos;
-      MermManipuleo = 0.25 + mermaVolatil;
-      MermaManipuleo.textContent = MermManipuleo;
-      KManipuleo = (kilos * MermManipuleo) / 100;
-      KilosManipuleo.textContent = KManipuleo;
-      TMermaKilos = (MKilos + KManipuleo);
-      TotalMermaKilos.textContent = TMermaKilos;
-      KilosNetos.textContent = Math.ceil(kilos - TMermaKilos);
-      KNetos = Math.ceil(kilos - TMermaKilos);
-      nMerma = document.getElementById('TotalMermaKilos').innerHTML;
+}
+function calculoTrigo() {
+   datos();
+   fetch('cereales.json')
+      .then((res) => res.json())
+      .then((data) => {
+         let bus = data.find(elemento => elemento.humedadtrigo == humedadselec);
+         const merma = bus.rebajatrigo;
+         Porcentaje.textContent = merma;
+         MKilos = (Math.ceil(kilos * merma) / 100);
+         MermManipuleo = 0.10 + mermaVolatil;
+         porcentajes();
       }
-   )}
+      )
+}
+function humedadMaiz() {
+
+   if (humedadselec < 14.6) {
+
+      Swal.fire({
+         icon: 'error',
+         title: 'Este camion se encuentra dentro de la tolerancia admitida - Ingrese un valor entre 14.6 - 25'
+      })
+   } else if (humedadselec > 25) {
+      Swal.fire({
+         icon: 'error',
+         title: 'Este camion supera el maximo permitido de humedad.'
+      })
+   }
+   if (volatil.checked == true) {
+      mermaVolatil = 0.30;
+      calculoMaiz();
+   } else {
+      mermaVolatil = 0;
+      calculoMaiz();
+   }
+}
+function calculoMaiz() {
+   datos();
+   fetch('cereales.json')
+      .then((res) => res.json())
+      .then((data) => {
+         let bus = data.find(elemento => elemento.humedadmaiz == humedadselec);
+         const merma = bus.rebajamaiz;
+         Porcentaje.textContent = merma;
+         MKilos = (Math.ceil(kilos * merma) / 100);
+         MermManipuleo = 0.25 + mermaVolatil;
+         porcentajes();
+      }
+      )
+}
+function datos() {
+   volatil = document.getElementById('volatil');
+   kilos = document.querySelector("#kilos").value;
+   humedadselec = document.getElementById('humedad').value;
+   cereal = document.querySelector("#cereales").value;
+}
+
+function porcentajes() {
+   MermaKilos.textContent = MKilos;
+   MermaManipuleo.textContent = MermManipuleo;
+   KManipuleo = (kilos * MermManipuleo) / 100;
+   KilosManipuleo.textContent = KManipuleo;
+   TMermaKilos = (MKilos + KManipuleo);
+   TotalMermaKilos.textContent = TMermaKilos;
+   KilosNetos.textContent = Math.ceil(kilos - TMermaKilos);
+   KNetos = Math.ceil(kilos - TMermaKilos);
+   nMerma = document.getElementById('TotalMermaKilos').innerHTML;
+
+}
--- js/main2.js
+++ js/main2.js
@@ -1,12 +1,47 @@
 document.querySelector('#agregar').addEventListener('click', agregar);
-//document.querySelector('#borrar').addEventListener('click', borrarRegistro)
-window.addEventListener("load", registro) 
+window.addEventListener("load", registro)
+
+window.onload = function () {
+    TSComponents.Card.render('trigo', {
+        serieId: '369.2_TRIGOIGO__5',
+        color: '#F9A822',
+        title: "Embarques de TRIGO",
+        links: "none",
+        apiBaseUrl: 'http://apis.datos.gob.ar/series/api'
+    })
+
+    TSComponents.Card.render('soja', {
+        serieId: '369.2_POROTO_SOJOJA__11',
+        color: '#6EA100',
+        explicitSign: true,
+        title: "Embarques de Poroto de SOJA",
+        links: "none",
+        apiBaseUrl: 'http://apis.datos.gob.ar/series/api'
+    })
+
+    TSComponents.Card.render('maiz', {
+        serieId: '369.3_MAIZAIZ__4',
+        color: '#f1dd50',
+        explicitSign: true,
+        title: "Embarques de MAIZ",
+        links: "none",
+        apiBaseUrl: 'http://apis.datos.gob.ar/series/api'
+    })
+
+    TSComponents.Card.render('embarques', {
+        serieId: '369.3_TOTALTAL__5',
+        color: '#f1dd50',
+        explicitSign: true,
+        title: "Embarques de Total",
+        links: "none",
+        apiBaseUrl: 'http://apis.datos.gob.ar/series/api'
+    })
+}
+
 let aNuevoCamion = [];
 
 moment.locale("es");
 let fecha = moment().format('Do MMM YY');
-console.log(fecha);
-
 
 function agregar() {
     CartaPorte = document.getElementById('CP').value;
@@ -20,8 +55,6 @@ function agregar() {
     localStorage.setItem('lista', JSON.stringify(aNuevoCamion));
 
     const tablaCamiones = document.getElementById('listadoCamiones');
-    console.log(aNuevoCamion)
-
     let fila = tablaCamiones.insertRow(0 + 1);
     let celda0 = fila.insertCell(0);
     let celda1 = fila.insertCell(1);
@@ -31,8 +64,8 @@ function agregar() {
     let celda5 = fila.insertCell(5);
     let celda6 = fila.insertCell(6);
 
-    
-    celda0.innerHTML = cereal; 
+
+    celda0.innerHTML = cereal;
     celda1.innerHTML = CartaPorte;
     celda2.innerHTML = Kilos;
     celda3.innerHTML = Humedad;
@@ -43,13 +76,13 @@ function agregar() {
 }
 
 function registro() {
-    
+
     if (localStorage.getItem('lista') !== null) {
         aNuevoCamion = JSON.parse(localStorage.getItem('lista'));
-    } 
-    if (aNuevoCamion.length > 0){
-            const tablaCamiones = document.getElementById('listadoCamiones');
-            aNuevoCamion.forEach(({cereal, CartaPorte, Kilos, Humedad, Merma, KilosNeto}) => {
+    }
+    if (aNuevoCamion.length > 0) {
+        const tablaCamiones = document.getElementById('listadoCamiones');
+        aNuevoCamion.forEach(({ cereal, CartaPorte, Kilos, Humedad, Merma, KilosNeto }) => {
             let fila = tablaCamiones.insertRow(0 + 1);
             let celda0 = fila.insertCell(0);
             let celda1 = fila.insertCell(1);
@@ -65,13 +98,13 @@ function registro() {
             celda3.innerHTML = Humedad;
             celda4.innerHTML = Merma;
             celda5.innerHTML = KilosNeto;
-            celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');    
+            celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');
         })
         const newButtons = document.querySelectorAll('#borrarRegistro')
         newButtons.forEach((button) => {
             button.addEventListener('click', confirmacion);
         })
-    }  
+    }
 
 }
 function confirmacion() {
@@ -81,57 +114,45 @@ function confirmacion() {
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        heightAuto:'true'
-        
-      }).then((result) => {
-        if (result.isConfirmed) {eliminar()
-          Swal.fire(
-            'Elminado!',
-            'El registro ha sido eliminado.',
-            'success',
-            
-          )
-        }
-      })
-    //let resultado = window.confirm("Esta seguro que quiere eliminar este registro");
-    //if (resultado === true) {
-        
-    
+        heightAuto: 'true'
 
+    }).then((result) => {
+        if (result.isConfirmed) {
+            eliminar()
+            Swal.fire(
+                'Elminado!',
+                'El registro ha sido eliminado.',
+                'success',
+
+            )
+        }
+    })
 }
-function eliminar() { 
+function eliminar() {
     let indice = aNuevoCamion.indexOf();
     if (indice == -1) {
         aNuevoCamion.splice(indice, 1);
         document.getElementById('listadoCamiones').deleteRow(indice);
         localStorage.setItem('lista', JSON.stringify(aNuevoCamion))
     }
-    }
-/* 
-    function borrarRegistro() {
-    let resultado = window.confirm("Esta seguro que quiere eliminar el historial");
-    if (resultado === true) {
-        
-        localStorage.clear()
-        
-    } */
-//}
-function tabla (){
+}
+function tabla() {
     const tablaCamiones = document.getElementById('listadoCamiones');
     let fila = tablaCamiones.insertRow(0 + 1);
-            let celda0 = fila.insertCell(0);
-            let celda1 = fila.insertCell(1);
-            let celda2 = fila.insertCell(2);
-            let celda3 = fila.insertCell(3);
-            let celda4 = fila.insertCell(4);
-            let celda5 = fila.insertCell(5);
-            let celda6 = fila.insertCell(6);
+    let celda0 = fila.insertCell(0);
+    let celda1 = fila.insertCell(1);
+    let celda2 = fila.insertCell(2);
+    let celda3 = fila.insertCell(3);
+    let celda4 = fila.insertCell(4);
+    let celda5 = fila.insertCell(5);
+    let celda6 = fila.insertCell(6);
 
-            celda0.innerHTML = cereal;
-            celda1.innerHTML = CartaPorte;
-            celda2.innerHTML = Kilos;
-            celda3.innerHTML = Humedad;
-            celda4.innerHTML = Merma;
-            celda5.innerHTML = KilosNeto;
-            celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');   
+    celda0.innerHTML = cereal;
+    celda1.innerHTML = CartaPorte;
+    celda2.innerHTML = Kilos;
+    celda3.innerHTML = Humedad;
+    celda4.innerHTML = Merma;
+    celda5.innerHTML = KilosNeto;
+    celda6.innerHTML = ('<img src="IMG/remove.png" width="20px" height="20px" id="borrarRegistro"> </img>');
 }
+
